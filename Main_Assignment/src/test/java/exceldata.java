82a0c798aa0f2b814faef923958892c0728ca9e3
ravi123kiran <ravikumaa@deloitte.com>
2022-04-04 11:46:29 +0530

    classes added

---
+++ Main_Assignment/src/test/java/exceldata.java
@@ -0,0 +1,92 @@
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+    public class exceldata {
+        public String getString(int sh,int row, int column) throws IOException {
+            String excelPath = "C:\\Users\\ravikumaa\\Desktop\\Finaldata.xlsx";
+            FileInputStream fis = new FileInputStream(excelPath);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(sh);
+            XSSFRow r = null;
+            XSSFCell cell = null;
+            String st= null;
+            r = sheet.getRow(row);
+            cell = r.getCell(column);
+            if(column == 2){
+                int a =(int) cell.getNumericCellValue();
+                st=Integer.toString(a);
+                return st;
+            }
+            st = cell.getStringCellValue();
+            return st;
+        }
+
+
+        public int getAge(int sh, int row, int column) throws IOException {
+            String excelPath = "C:\\Users\\ravikumaa\\Desktop\\Finaldata.xlsx";
+            FileInputStream fis = new FileInputStream(excelPath);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(sh);
+            XSSFRow r = null;
+            XSSFCell cell = null;
+            r = sheet.getRow(row);
+            cell = r.getCell(column);
+            int ag = (int) cell.getNumericCellValue();
+            return ag;
+        }
+
+
+        public void writeToken(Object ObjToken,int row, int column) throws IOException {
+            String excelPath = "C:\\Users\\ravikumaa\\Desktop\\Finaldata.xlsx";
+            FileInputStream fis = new FileInputStream(excelPath);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            XSSFRow r = null;
+            XSSFCell cell = null;
+            r= sheet.getRow(row);
+            cell = r.createCell(column);
+            cell.setCellType(CellType.STRING);
+            String token =(String)ObjToken;
+            cell.setCellValue(token);
+            FileOutputStream fos = new FileOutputStream(excelPath);
+            wb.write(fos);
+            fos.close();
+        }
+        public String getToken(int sht,int row,int col) throws IOException {
+            String excelPath = "C:\\Users\\ravikumaa\\Desktop\\Finaldata.xlsx";
+            FileInputStream fis = new FileInputStream(excelPath);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(sht);
+            XSSFRow r = null;
+            XSSFCell cell = null;
+            String st= null;
+            r = sheet.getRow(row);
+            cell = r.getCell(col);
+            st = cell.getStringCellValue();
+            return st;
+        }
+        public String getString1(int row, int column) throws IOException {
+            String excelPath = "C:\\Users\\ravikumaa\\Desktop\\Finaldata.xlsx";
+            FileInputStream fis = new FileInputStream(excelPath);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            XSSFRow r = null;
+            XSSFCell cell = null;
+            String st= null;
+            r = sheet.getRow(row);
+            cell = r.getCell(column);
+            if(column == 2){
+                int a =(int) cell.getNumericCellValue();
+                st=Integer.toString(a);
+                return st;
+            }
+            st = cell.getStringCellValue();
+            return st;
+        }
+    }
+
--- Main_Assignment/src/test/java/task_1.java
+++ Main_Assignment/src/test/java/task_1.java
@@ -1,25 +1,55 @@
-import org.json.JSONArray;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
-import io.restassured.response.Response;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import java.io.File;
+import java.io.IOException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class task_1 {
-    @Test
-    public void Request1() {
-        RequestSpecification request = RestAssured.given();
-        // Setting Base URI
-        request.baseUri("https://restful-booker.herokuapp.com");
-        // Setting Base Path
-        request.basePath("/booking");
+    @Test(priority = 1)
+    public void userRegister() throws IOException {
 
-        Response response = request.get();
+        for (int i = 1; i <= 5; i++) {
+            exceldata ed = new exceldata();
+            String nam = ed.getString(0,i, 0);
+            String emai = ed.getString(0,i, 1);
+            String pass = ed.getString(0,i, 2);
+            int age = ed.getAge(0,i, 3);
+            Data dt = new Data(nam, emai, pass, age);
+            Response response=given().
+                    body(dt).
+                    contentType(ContentType.JSON).
+                    when().
+                    post("https://api-nodejs-todolist.herokuapp.com/user/register").
+                    then().
+                    log().body().
 
-        System.out.println(response.asString());
+                    statusCode(HttpStatus.SC_CREATED).extract().response();
+            JSONObject jsonObject = new JSONObject(response.asString());
+            Object ObjToken = jsonObject.get("token");
+            exceldata excelData = new exceldata();
+            excelData.writeToken(ObjToken,i,4);
+        }
+    }
+    @Test(priority = 2)
+    public void userLogin() throws IOException {
+        exceldata ed = new exceldata();
+        String emai = ed.getString(0,1, 1);
+        String pass = ed.getString(0,1, 2);
+        Data dt = new Data(emai, pass);
+        Response response = given().
+                body(dt).
+                contentType(ContentType.JSON).
+                when().
+                post("https://api-nodejs-todolist.herokuapp.com/user/login").
+                then().
+                log().body().
+                statusCode(HttpStatus.SC_OK).extract().response();
+        JSONObject jsonObject = new JSONObject(response.asString());
+        Object obj = jsonObject.getJSONObject("user").get("email");
+        assertThat(obj, is(emai));
     }
 }
